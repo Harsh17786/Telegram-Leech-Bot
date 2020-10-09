8b7608dd7aa873357841e1092c6590ce69917f38
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-10-09 15:13:52 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,11 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1268fill---your----data"
-	APP_ID = 1373938
-	API_HASH = "fill--your--data"
-	OWNER_ID = "" #ID of bot owner
-	AUTH_CHANNEL = [-10082786282972]
+	TG_BOT_TOKEN= "1348362528:AAFydvRB4akZgiF77yT_vunHQ-06rOEVbik"
+	APP_ID = 1570274
+	API_HASH = "d463fbcaa44274b3e969028dd570d3ab"
+	OWNER_ID = "" 992712818
+	AUTH_CHANNEL = [-1001352119320]
 	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
 	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
